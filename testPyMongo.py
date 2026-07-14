32972ac1b5dcc090201a32af79d55b8e432c3ca3
Jorge <jltamayocabello@gmail.com>
2026-07-14 14:11:45 -0400

    Se imprime usuarios en la colección y se crea nuevo usuario

--- testPyMongo.py
+++ testPyMongo.py
@@ -5,3 +5,16 @@ db = client["miBaseDeDatos"]
 collection = db["usuarios"]
 
 print ("Colecciones de la base de datos:", db.list_collection_names())
+
+print("Usuarios en la colección")
+for usuario in collection.find():
+    print(usuario)
+
+# Usuario Nuevo
+nuevo_usuario = {
+    "nombre": "Pedro Python",
+    "edad": 30,
+    "intereses": ["Python", "MongoDB", "Django"]
+}
+resultado = collection.insert_one(nuevo_usuario)
+print("Usuario insertado;", resultado.inserted_id)
